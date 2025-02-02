4de2b7482961d7bf578beec96c37684f00ad2007
Kodam Ashwini <kodamashwini2003@gmail.com>
2025-02-02 09:36:59 +0530

    java

---
+++ src/main/java/com/ashwini/automobile_management_system/VehicleManagement.java
@@ -0,0 +1,11 @@
+package com.ashwini.automobile_management_system;
+
+public interface VehicleManagement {
+	void createAccount();
+	void addCustomer();
+	void login();
+	void addVehicle();
+	void removeVehicle();
+	void updateVehicle();
+	void displayAllVehicles();
+}
